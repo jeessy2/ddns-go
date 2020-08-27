32ac93848b54f8151618296f478a59868c835734
jie <jie@jiedeiMac.local>
2020-08-27 17:38:17 +0800

    web support

---
+++ util/openExplorer.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+// OpenExplorer 打开本地浏览器
+func OpenExplorer(url string) {
+	var cmd string
+	var args []string
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "explorer"
+		args = []string{"/c", "start"}
+	case "darwin":
+		// mac
+		cmd = "open"
+	default:
+		// linux
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+
+	err := exec.Command(cmd, args...).Start()
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("成功打开浏览器")
+	}
+}
