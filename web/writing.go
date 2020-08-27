f38fd8a4d7974dc156160877808d20963f41fa2f
jeessy2 <6205259+jeessy2@users.noreply.github.com>
2020-08-27 17:38:17 +0800

    web support

---
+++ web/writing.go
@@ -0,0 +1,50 @@
+package web
+
+import (
+	"ddns-go/config"
+	"ddns-go/util"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Writing 步骤二，填写信息
+func Writing(writer http.ResponseWriter, request *http.Request) {
+	tmpl, err := template.ParseFiles("./static/pages/writing.html")
+	if err != nil {
+		fmt.Println("Error happened..")
+		fmt.Println(err)
+		return
+	}
+
+	conf := &config.Config{}
+
+	// 解析文件
+	var configFile string = util.GetConfigFilePath()
+	_, err = os.Stat(configFile)
+	if err == nil {
+		// 不为空，解析文件
+		byt, err := ioutil.ReadFile(configFile)
+		if err == nil {
+			err = yaml.Unmarshal(byt, conf)
+			if err == nil {
+				tmpl.Execute(writer, conf)
+				return
+			}
+		}
+	}
+
+	// 默认值
+	if conf.Ipv4.URL == "" {
+		conf.Ipv4.URL = "https://api-ipv4.ip.sb/ip"
+	}
+	if conf.Ipv6.URL == "" {
+		conf.Ipv4.URL = "https://api-ipv6.ip.sb/ip"
+	}
+
+	tmpl.Execute(writer, nil)
+}
