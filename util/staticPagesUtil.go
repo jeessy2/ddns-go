1bd7506d7ddba084b83285bb970617cce0d854f2
jie <jie@jiedeiMac.local>
2020-08-28 10:26:04 +0800

    static data

---
+++ util/staticPagesUtil.go
@@ -0,0 +1,20 @@
+package util
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+// GetStaticResourcePath 获得静态资源文件路径
+func GetStaticResourcePath(orgPath string) (temPath string, err error) {
+	data, err := Asset(orgPath)
+	if err != nil {
+		log.Println("Asset was not found.")
+		return "", err
+	}
+	tempFile := os.TempDir() + strings.ReplaceAll(orgPath, "/", "_")
+	ioutil.WriteFile(tempFile, data, 0600)
+	return tempFile, nil
+}
