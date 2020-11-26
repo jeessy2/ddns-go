510b19fab365e2f151418048c76c1974d0d59591
herbertqiao <herbertqiao@home.catofes.com>
2020-11-26 15:50:09 +0800

    Add more flags.

--- main.go
+++ main.go
@@ -6,14 +6,19 @@ import (
 	"ddns-go/static"
 	"ddns-go/util"
 	"ddns-go/web"
+	"flag"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 const port = "9876"
 
 func main() {
+	listen := flag.String("l", ":9876", "web server listen address")
+	every := flag.String("f", "300", "dns update frequency in second")
+	flag.Parse()
 	// 启动静态文件服务
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(static.AssetFile())))
 	http.Handle("/favicon.ico", http.StripPrefix("/", http.FileServer(static.AssetFile())))
@@ -28,12 +33,16 @@ func main() {
 		go util.OpenExplorer("http://127.0.0.1:" + port)
 	}
 
-	log.Println("启动端口", port, "...")
+	log.Println("监听", *listen, "...")
 
 	// 定时运行
-	go dns.RunTimer()
+	delay, err := strconv.ParseUint(*every, 10, 64)
+	if err != nil {
+		delay = 300
+	}
+	go dns.RunTimer(time.Duration(delay) * time.Second)
 
-	err = http.ListenAndServe(":"+port, nil)
+	err = http.ListenAndServe(*listen, nil)
 
 	if err != nil {
 		log.Println("启动端口发生异常, 1分钟后自动关闭此窗口", err)
