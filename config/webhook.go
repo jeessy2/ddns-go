a5a07d22864583e2eb6589824898a604661658c3
jeessy2 <6205259+jeessy2@users.noreply.github.com>
2020-12-24 10:01:04 +0800

    Optimize webhook code

--- config/webhook.go
+++ config/webhook.go
@@ -1,7 +1,105 @@
 package config
 
+import (
+	"ddns-go/util"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
 // Webhook Webhook
 type Webhook struct {
 	WebhookURL         string
 	WebhookRequestBody string
+}
+
+// updateStatusType 更新状态
+type updateStatusType string
+
+const (
+	// UpdatedNothing 未改变
+	UpdatedNothing updateStatusType = "未改变"
+	// UpdatedFailed 更新失败
+	UpdatedFailed = "失败"
+	// UpdatedSuccess 更新成功
+	UpdatedSuccess = "成功"
+)
+
+// ExecWebhook 添加或更新IPV4/IPV6记录
+func ExecWebhook(domains *Domains, conf *Config) {
+	v4Status := getDomainsStatus(domains.Ipv4Domains)
+	v6Status := getDomainsStatus(domains.Ipv6Domains)
+
+	if conf.WebhookURL != "" && (v4Status != UpdatedNothing || v6Status != UpdatedNothing) {
+		// 成功和失败都要触发webhook
+		method := "GET"
+		postPara := ""
+		contentType := "application/x-www-form-urlencoded"
+		if conf.WebhookRequestBody != "" {
+			method = "POST"
+			postPara = replacePara(domains, conf.WebhookRequestBody, v4Status, v6Status)
+			contentType = "application/json"
+		}
+		requestURL := replacePara(domains, conf.WebhookURL, v4Status, v6Status)
+		req, err := http.NewRequest(method, requestURL, strings.NewReader(postPara))
+		req.Header.Add("content-type", contentType)
+
+		clt := http.Client{}
+		clt.Timeout = 30 * time.Second
+		resp, err := clt.Do(req)
+		body, err := util.GetHTTPResponseOrg(resp, requestURL, err)
+		if err == nil {
+			log.Println(fmt.Sprintf("Webhook调用成功, 返回数据: %s", string(body)))
+		} else {
+			log.Println(fmt.Sprintf("Webhook调用失败，Err：%s", err))
+		}
+	}
+}
+
+// getDomainsStr 用逗号分割域名
+func getDomainsStatus(domains []*Domain) updateStatusType {
+	successNum := 0
+	for _, v46 := range domains {
+		switch v46.UpdateStatus {
+		case UpdatedFailed:
+			// 一个失败，全部失败
+			return UpdatedFailed
+		case UpdatedSuccess:
+			successNum++
+		}
+	}
+
+	if successNum > 0 {
+		// 迭代完成后一个成功，就成功
+		return UpdatedSuccess
+	}
+	return UpdatedNothing
+}
+
+// replacePara 替换参数
+func replacePara(domains *Domains, orgPara string, ipv4Result updateStatusType, ipv6Result updateStatusType) (newPara string) {
+	orgPara = strings.ReplaceAll(orgPara, "#{ipv4Addr}", domains.Ipv4Addr)
+	orgPara = strings.ReplaceAll(orgPara, "#{ipv4Result}", string(ipv4Result))
+	orgPara = strings.ReplaceAll(orgPara, "#{ipv4Domains}", getDomainsStr(domains.Ipv4Domains))
+
+	orgPara = strings.ReplaceAll(orgPara, "#{ipv6New}", domains.Ipv6Addr)
+	orgPara = strings.ReplaceAll(orgPara, "#{ipv6Result}", string(ipv6Result))
+	orgPara = strings.ReplaceAll(orgPara, "#{ipv6Domains}", getDomainsStr(domains.Ipv6Domains))
+
+	return orgPara
+}
+
+// getDomainsStr 用逗号分割域名
+func getDomainsStr(domains []*Domain) string {
+	str := ""
+	for i, v46 := range domains {
+		str += v46.String()
+		if i != len(domains)-1 {
+			str += ","
+		}
+	}
+
+	return str
 }
