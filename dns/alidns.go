cf45b0e55301a24cde168357fc02a99426c47196
jeessy2 <454207171@qq.com>
2021-02-25 16:49:16 +0800

    refact: alidns

--- dns/alidns.go
+++ dns/alidns.go
@@ -1,28 +1,47 @@
 package dns
 
 import (
+	"bytes"
 	"ddns-go/config"
+	"ddns-go/util"
 	"log"
-
-	alidnssdk "github.com/aliyun/alibaba-cloud-sdk-go/services/alidns"
+	"net/http"
+	"net/url"
+	"time"
 )
 
-// Alidns 阿里云dns实现
+const (
+	alidnsEndpoint string = "https://alidns.aliyuncs.com/"
+)
+
+// Alidns Alidns
 type Alidns struct {
-	client  *alidnssdk.Client
-	Domains config.Domains
+	DNSConfig config.DNSConfig
+	Domains   config.Domains
+}
+
+// AlidnsSubDomainRecords 记录
+type AlidnsSubDomainRecords struct {
+	TotalCount    int
+	DomainRecords struct {
+		Record []struct {
+			DomainName string
+			RecordID   string
+			Value      string
+		}
+	}
+}
+
+// AlidnsResp 修改添加返回结果
+type AlidnsResp struct {
+	RecordID  string
+	RequestID string
 }
 
 // Init 初始化
 func (ali *Alidns) Init(conf *config.Config) {
-	client, err := alidnssdk.NewClientWithAccessKey("cn-hangzhou", conf.DNS.ID, conf.DNS.Secret)
-	if err != nil {
-		log.Println("Alidns链接失败")
-	}
-	ali.client = client
-
+	ali.DNSConfig = conf.DNS
 	ali.Domains.ParseDomain(conf)
-
 }
 
 // AddUpdateDomainRecords 添加或更新IPv4/IPv6记录
@@ -39,56 +58,100 @@ func (ali *Alidns) addUpdateDomainRecords(recordType string) {
 		return
 	}
 
-	existReq := alidnssdk.CreateDescribeSubDomainRecordsRequest()
-	existReq.Type = recordType
-
 	for _, domain := range domains {
-		existReq.SubDomain = domain.GetFullDomain()
-		rep, err := ali.client.DescribeSubDomainRecords(existReq)
-		if err != nil {
-			domain.UpdateStatus = config.UpdatedFailed
-			log.Println(err)
-		}
-		if rep.TotalCount > 0 {
-			// Update
-			for _, record := range rep.DomainRecords.Record {
-				if record.Value == ipAddr {
-					log.Printf("你的IP %s 没有变化, 域名 %s", ipAddr, domain)
-					continue
-				}
-				request := alidnssdk.CreateUpdateDomainRecordRequest()
-				request.Scheme = "https"
-				request.Value = ipAddr
-				request.Type = recordType
-				request.RR = domain.GetSubDomain()
-				request.RecordId = record.RecordId
 
-				updateResp, err := ali.client.UpdateDomainRecord(request)
-				if err == nil && updateResp.BaseResponse.IsSuccess() {
-					log.Printf("更新域名解析 %s 成功！IP: %s", domain, ipAddr)
-					domain.UpdateStatus = config.UpdatedSuccess
-				} else {
-					log.Printf("更新域名解析 %s 失败！IP: %s, Error: %s, Response: %s", domain, ipAddr, err, updateResp.GetHttpContentString())
-					domain.UpdateStatus = config.UpdatedFailed
-				}
-			}
-		} else {
-			// Add
-			request := alidnssdk.CreateAddDomainRecordRequest()
-			request.Scheme = "https"
-			request.Value = ipAddr
-			request.Type = recordType
-			request.RR = domain.GetSubDomain()
-			request.DomainName = domain.DomainName
+		var record AlidnsSubDomainRecords
+		// 获取当前域名信息
+		// url := alidnsEndpoint + fmt.Sprintf("?Action=%s&DomainName=%s", "DescribeSubDomainRecords", domain.GetFullDomain())
+		params := url.Values{}
+		params.Set("Action", "DescribeSubDomainRecords")
+		params.Set("SubDomain", domain.GetFullDomain())
+		params.Set("Type", recordType)
+		err := ali.request(params, &record)
 
-			createResp, err := ali.client.AddDomainRecord(request)
-			if err == nil && createResp.BaseResponse.IsSuccess() {
-				log.Printf("新增域名解析 %s 成功！IP: %s", domain, ipAddr)
-				domain.UpdateStatus = config.UpdatedSuccess
+		if err == nil {
+			if record.TotalCount > 0 {
+				// 存在，更新
+				ali.modify(record, domain, recordType, ipAddr)
 			} else {
-				log.Printf("新增域名解析 %s 失败！IP: %s, Error: %s, Response: %s", domain, ipAddr, err, createResp.GetHttpContentString())
-				domain.UpdateStatus = config.UpdatedFailed
+				// 不存在，创建
+				ali.create(domain, recordType, ipAddr)
 			}
 		}
+
 	}
+}
+
+// 创建
+func (ali *Alidns) create(domain *config.Domain, recordType string, ipAddr string) {
+	params := url.Values{}
+	params.Set("Action", "AddDomainRecord")
+	params.Set("DomainName", domain.DomainName)
+	params.Set("RR", domain.GetSubDomain())
+	params.Set("Type", recordType)
+	params.Set("Value", ipAddr)
+
+	var result AlidnsResp
+	err := ali.request(params, &result)
+
+	if err == nil && "" != result.RecordID {
+		log.Printf("新增域名解析 %s 成功！IP: %s", domain, ipAddr)
+		domain.UpdateStatus = config.UpdatedSuccess
+	} else {
+		log.Printf("新增域名解析 %s 失败！", domain)
+		domain.UpdateStatus = config.UpdatedFailed
+	}
+}
+
+// 修改
+func (ali *Alidns) modify(record AlidnsSubDomainRecords, domain *config.Domain, recordType string, ipAddr string) {
+
+	// 相同不修改
+	if len(record.DomainRecords.Record) > 0 && record.DomainRecords.Record[0].Value == ipAddr {
+		log.Printf("你的IP %s 没有变化, 域名 %s", ipAddr, domain)
+		return
+	}
+
+	params := url.Values{}
+	params.Set("Action", "UpdateDomainRecord")
+	params.Set("RR", domain.GetSubDomain())
+	params.Set("RecordId", record.DomainRecords.Record[0].RecordID)
+	params.Set("Type", recordType)
+	params.Set("Value", ipAddr)
+
+	var result AlidnsResp
+	err := ali.request(params, &result)
+
+	if err == nil && "" != result.RecordID {
+		log.Printf("更新域名解析 %s 成功！IP: %s", domain, ipAddr)
+		domain.UpdateStatus = config.UpdatedSuccess
+	} else {
+		log.Printf("更新域名解析 %s 失败！", domain)
+		domain.UpdateStatus = config.UpdatedFailed
+	}
+}
+
+// request 统一请求接口
+func (ali *Alidns) request(params url.Values, result interface{}) (err error) {
+
+	util.AliyunSigner(ali.DNSConfig.ID, ali.DNSConfig.Secret, &params)
+
+	req, err := http.NewRequest(
+		"GET",
+		alidnsEndpoint,
+		bytes.NewBuffer(nil),
+	)
+	req.URL.RawQuery = params.Encode()
+
+	if err != nil {
+		log.Println("http.NewRequest失败. Error: ", err)
+		return
+	}
+
+	clt := http.Client{}
+	clt.Timeout = 30 * time.Second
+	resp, err := clt.Do(req)
+	err = util.GetHTTPResponse(resp, alidnsEndpoint, err, result)
+
+	return
 }
--- go.mod
+++ go.mod
@@ -2,11 +2,4 @@ module ddns-go
 
 go 1.16
 
-require (
-	github.com/aliyun/alibaba-cloud-sdk-go v1.61.439
-	github.com/jmespath/go-jmespath v0.3.0 // indirect
-	github.com/json-iterator/go v1.1.10 // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
-	gopkg.in/ini.v1 v1.60.1 // indirect
-	gopkg.in/yaml.v2 v2.3.0
-)
+require gopkg.in/yaml.v2 v2.3.0
