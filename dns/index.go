7420bca934dec5c6ab9d019203e05236019dfcf1
jie <454207171@qq.com>
2021-02-27 21:45:58 +0800

    feat: install ddns-go as a service

--- dns/index.go
+++ dns/index.go
@@ -13,7 +13,8 @@ type DNS interface {
 }
 
 // RunTimer 定时运行
-func RunTimer(delay time.Duration) {
+func RunTimer(firstDelay time.Duration, delay time.Duration) {
+	time.Sleep(firstDelay)
 	for {
 		RunOnce()
 		time.Sleep(delay)
