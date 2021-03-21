63b6862489e3b93e1e8bc17fddd6c158585b5e9e
jeessy2 <6205259+jeessy2@users.noreply.github.com>
2021-03-21 11:29:41 +0800

    fix: webhook failed logic

--- dns/dnspod.go
+++ dns/dnspod.go
@@ -63,7 +63,6 @@ func (dnspod *Dnspod) addUpdateDomainRecords(recordType string) {
 	for _, domain := range domains {
 		result, err := dnspod.getRecordList(domain, recordType)
 		if err != nil {
-			domain.UpdateStatus = config.UpdatedFailed
 			return
 		}
 
