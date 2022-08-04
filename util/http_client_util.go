627bd65901ab1828f4edc6a9aa9ad07fa5f9223a
jeessy2 <6205259+jeessy2@users.noreply.github.com>
2022-08-05 02:49:39 +0800

    fix: use tcp6 when get ipv6 from api

--- util/http_client_util.go
+++ util/http_client_util.go
@@ -15,26 +15,54 @@ func CreateHTTPClient() *http.Client {
 	}
 }
 
-var dialer = &net.Dialer{
+var noProxyDialer = &net.Dialer{
 	Timeout:   30 * time.Second,
 	KeepAlive: 30 * time.Second,
 }
 
-// CreateNoProxyHTTPClient CreateNoProxyHTTPClient
-func CreateNoProxyHTTPClient() *http.Client {
+var noProxyTcp4Transport = &http.Transport{
+	// no proxy
+	// DisableKeepAlives
+	DisableKeepAlives: true,
+	// tcp4
+	DialContext: func(ctx context.Context, network, address string) (net.Conn, error) {
+		return noProxyDialer.DialContext(ctx, "tcp4", address)
+	},
+	// from http.DefaultTransport
+	ForceAttemptHTTP2:     true,
+	MaxIdleConns:          100,
+	IdleConnTimeout:       90 * time.Second,
+	TLSHandshakeTimeout:   10 * time.Second,
+	ExpectContinueTimeout: 1 * time.Second,
+}
+
+var noProxyTcp6Transport = &http.Transport{
+	// no proxy
+	// DisableKeepAlives
+	DisableKeepAlives: true,
+	// tcp6
+	DialContext: func(ctx context.Context, network, address string) (net.Conn, error) {
+		return noProxyDialer.DialContext(ctx, "tcp6", address)
+	},
+	// from http.DefaultTransport
+	ForceAttemptHTTP2:     true,
+	MaxIdleConns:          100,
+	IdleConnTimeout:       90 * time.Second,
+	TLSHandshakeTimeout:   10 * time.Second,
+	ExpectContinueTimeout: 1 * time.Second,
+}
+
+// CreateNoProxyHTTPClient Create NoProxy HTTP Client
+func CreateNoProxyHTTPClient(network string) *http.Client {
+	if network == "tcp6" {
+		return &http.Client{
+			Timeout:   30 * time.Second,
+			Transport: noProxyTcp6Transport,
+		}
+	}
+
 	return &http.Client{
-		Timeout: 30 * time.Second,
-		Transport: &http.Transport{
-			// no proxy
-			// from http.DefaultTransport
-			DialContext: func(ctx context.Context, network, address string) (net.Conn, error) {
-				return dialer.DialContext(ctx, network, address)
-			},
-			ForceAttemptHTTP2:     true,
-			MaxIdleConns:          100,
-			IdleConnTimeout:       90 * time.Second,
-			TLSHandshakeTimeout:   10 * time.Second,
-			ExpectContinueTimeout: 1 * time.Second,
-		},
+		Timeout:   30 * time.Second,
+		Transport: noProxyTcp4Transport,
 	}
 }
