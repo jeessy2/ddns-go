jeessy2/ddns-go

commit 606b44a5ba1002ccaac1c60edcdcf5bd0b955510

feat: faster Docker multi-platform builds and shrink Go binaries (#498)
* feat(docker): faster multi-platform builds

See: https://www.docker.com/blog/faster-multi-platform-builds-dockerfile-cross-compilation-guide/

* feat(make): disable symbol table and DWARF generation

by adding -s and -w options to -ldflags

See: https://pkg.go.dev/cmd/link

Dockerfile
@@ -1,11 +1,14 @@
 # build stage
-FROM golang:1.19 AS builder
+FROM --platform=$BUILDPLATFORM golang:1.19-alpine AS builder
 
 WORKDIR /app
 COPY . .
-RUN go env -w GO111MODULE=on \
+ARG TARGETOS TARGETARCH
+RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories \
+    && apk add git make tzdata \
+    && go env -w GO111MODULE=on \
     && go env -w GOPROXY=https://goproxy.cn,direct \
-    && make clean build
+    && GOOS=$TARGETOS GOARCH=$TARGETARCH make clean build
 
 # final stage
 FROM alpine
@@ -13,8 +16,7 @@ LABEL name=ddns-go
 LABEL url=https://github.com/jeessy2/ddns-go
 
 WORKDIR /app
-RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories \
-    && apk add --no-cache tzdata
+COPY --from=builder /usr/share/zoneinfo /usr/share/zoneinfo
 ENV TZ=Asia/Shanghai
 COPY --from=builder /app/ddns-go /app/ddns-go
 EXPOSE 9876

Makefile
@@ -6,7 +6,7 @@ DIR_SRC=.
 DOCKER_CMD=docker
 
 GO_ENV=CGO_ENABLED=0
-GO_FLAGS=-ldflags="-X main.version=$(VERSION) -X 'main.buildTime=`date`' -extldflags -static" -trimpath
+GO_FLAGS=-ldflags="-X main.version=$(VERSION) -X 'main.buildTime=`date`' -extldflags -static -s -w" -trimpath
 GO=$(GO_ENV) $(shell which go)
 GOROOT=$(shell `which go` env GOROOT)
 GOPATH=$(shell `which go` env GOPATH)

commit 61a83505772cfbf575bd3925c91dac852b2e16ba

fix(make): build_docker_image failed
Looks like it fails after #498 because it requires buildkit, add `DOCKER_BUILDKIT=1` to enable buildkit to fix this

Also, add `--no-cache` to `apk add` to not cache the index locally, forgot to do that in #498.

Dockerfile
@@ -5,7 +5,7 @@ WORKDIR /app
 COPY . .
 ARG TARGETOS TARGETARCH
 
-RUN apk add git make tzdata \
+RUN apk add --no-cache git make tzdata \
     && GOOS=$TARGETOS GOARCH=$TARGETARCH make clean build
 
 # final stage

Makefile
@@ -3,7 +3,8 @@
 VERSION=$(shell git describe --tags `git rev-list --tags --max-count=1`)
 BIN=ddns-go
 DIR_SRC=.
-DOCKER_CMD=docker
+DOCKER_ENV=DOCKER_BUILDKIT=1
+DOCKER=$(DOCKER_ENV) docker
 
 GO_ENV=CGO_ENABLED=0
 GO_FLAGS=-ldflags="-X main.version=$(VERSION) -X 'main.buildTime=`date`' -extldflags -static -s -w" -trimpath
@@ -15,7 +16,7 @@ build: $(DIR_SRC)/main.go
 	@$(GO) build $(GO_FLAGS) -o $(BIN) $(DIR_SRC)
 
 build_docker_image:
-	@$(DOCKER_CMD) build -f ./Dockerfile -t ddns-go:$(VERSION) .
+	@$(DOCKER) build -f ./Dockerfile -t ddns-go:$(VERSION) .
 
 test:
 	@$(GO) test ./...