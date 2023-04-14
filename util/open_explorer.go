368b99660e62f0c392fdffb9ff061153d38bb9c8
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2023-04-14 17:34:37 +0800

    fix(open_explorer): direct start command when switching `GOOS`
    Also build arm64.

--- util/open_explorer.go
+++ util/open_explorer.go
@@ -1,3 +1,5 @@
+// based on https://gist.github.com/hyg/9c4afcd91fe24316cbf0
+
 package util
 
 import (
@@ -8,17 +10,18 @@ import (
 
 // OpenExplorer 打开本地浏览器
 func OpenExplorer(url string) {
-	var cmd *exec.Cmd
+	var err error
+
 	switch runtime.GOOS {
 	case "windows":
-		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
 	case "darwin": // macOS
-		cmd = exec.Command("open", url)
+		err = exec.Command("open", url).Start()
 	default: // Linux
-		cmd = exec.Command("xdg-open", url)
+		err = exec.Command("xdg-open", url).Start()
 	}
 
-	if err := cmd.Start(); err != nil {
+	if err != nil {
 		fmt.Printf("请手动打开浏览器并访问 %s 进行配置\n", url)
 	} else {
 		fmt.Println("成功打开浏览器, 请在网页中进行配置")
