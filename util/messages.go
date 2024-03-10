0f081d47dd14fc2587c1bc7c54e33fbdda550a9c
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2024-03-10 18:30:45 +0800

    fix(web): IPv4/6 cannot be enabled without domains
    Currently, if IPv4/6 is enabled but there is no domain, nothing happens. In this case, we should throw an error.

--- util/messages.go
+++ util/messages.go
@@ -57,6 +57,8 @@ func init() {
 	message.SetString(language.English, "修改 '通过命令获取' 必须设置帐号密码，请先设置帐号密码", "Modify 'Get by command' must set username/password, please set username/password first")
 	message.SetString(language.English, "密码不安全！尝试使用更长的密码", "insecure password, try using a longer password")
 	message.SetString(language.English, "数据解析失败, 请刷新页面重试", "Data parsing failed, please refresh the page and try again")
+	message.SetString(language.English, "启用 IPv4 时 Domains 不能为空", "Domains cannot be empty when IPv4 is enabled")
+	message.SetString(language.English, "启用 IPv6 时 Domains 不能为空", "Domains cannot be empty when IPv6 is enabled")
 
 	// config
 	message.SetString(language.English, "从网卡获得IPv4失败", "Get IPv4 from network card failed")
