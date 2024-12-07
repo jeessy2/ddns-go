e0dc641faefa350af644381b5dd8bc43a211b0db
Secriy <secriyal@gmail.com>
2024-12-07 20:20:43 +0800

    fix: escape URL path to prevent issues with unescaped characters (#1334)

--- config/webhook.go
+++ config/webhook.go
@@ -74,7 +74,7 @@ func ExecWebhook(domains *Domains, conf *Config) (v4Status updateStatusType, v6S
 			util.Log("Webhook配置中的URL不正确")
 			return
 		}
-		req, err := http.NewRequest(method, fmt.Sprintf("%s://%s%s?%s", u.Scheme, u.Host, u.Path, u.Query().Encode()), strings.NewReader(postPara))
+		req, err := http.NewRequest(method, fmt.Sprintf("%s://%s%s?%s", u.Scheme, u.Host, u.EscapedPath(), u.Query().Encode()), strings.NewReader(postPara))
 		if err != nil {
 			util.Log("Webhook调用失败! 异常信息：%s", err)
 			return
