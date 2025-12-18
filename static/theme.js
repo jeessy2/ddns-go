58c5ec9a933863401d8678338e4cc84af94ecd6c
jeessy2 <6205259+jeessy2@users.noreply.github.com>
2025-12-18 19:07:35 +0800

    Revert "feat: add color-scheme support for better dark theme detection (#1597)"
    This reverts commit 5e552c7d442e0421c8333ef0b759f2913b7c6695.

--- static/theme.js
+++ static/theme.js
@@ -1,14 +1,10 @@
 function toggleTheme(write = false) {
   const docEle = document.documentElement;
-  const metaTag = document.getElementById("color-scheme-meta");
-
   if (docEle.getAttribute("data-theme") === "dark") {
     docEle.removeAttribute("data-theme");
-    if (metaTag) metaTag.setAttribute("content", "light dark");
     write && localStorage.setItem("theme", "light");
   } else {
     docEle.setAttribute("data-theme", "dark");
-    if (metaTag) metaTag.setAttribute("content", "dark light");
     write && localStorage.setItem("theme", "dark");
   }
 }
